6265d42677857707579dee4cbb829cd9bb8c9402
johannesbakken <johannes.e.bakken@hotmail.com>
2025-12-05 14:41:38 +0100

    added error function to ghub api call

--- api.js
+++ api.js
@@ -2,5 +2,9 @@ $.ajax({
     url: 'https://api.github.com/repos/johannesbakken/testrepo',
     success: function(response){
         console.log(response);
+    },
+    error: function(response){
+        console.log(`Status: ${response}.`);
     }
+    
 })
